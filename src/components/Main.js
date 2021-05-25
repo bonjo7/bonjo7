f36b4263f45827a535753077690ae2dc7001897e
Bernard Thompson <bethomps@redhat.com>
2021-05-25 11:52:04 +0100

    fix: updated css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,12 @@
 import React from "react";
+import styles from "./Main.module.css"
 import logo from "../images/Doge.png";
 
 const MainPage = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <img src={logo} className={styles.AppLogo} alt='logo' />
         <p>
           <b>Much WOW - </b>DF7QpRhieXw6KE6QXzy9W2rk3u4nNZPgNw
         </p>
