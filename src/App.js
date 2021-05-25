1b8028f128d8707b08a5ef8890eb7bae22c20e4c
Bernard Thompson <bethomps@redhat.com>
2021-05-25 11:47:55 +0100

    feat: added main component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import logo from "./images/Doge.png";
-import './App.css';
+import MainPage from "./components/Main"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <b>Much WOW - </b>DF7QpRhieXw6KE6QXzy9W2rk3u4nNZPgNw
-        </p>
-      </header>
-    </div>
+    <MainPage/>
   );
 }
 
---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "../images/Doge.png";
+
+const MainPage = () => {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          <b>Much WOW - </b>DF7QpRhieXw6KE6QXzy9W2rk3u4nNZPgNw
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default MainPage;
