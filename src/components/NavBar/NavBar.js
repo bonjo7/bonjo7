148c3fbee29904abe5d77a266e137072b39dc1d3
Bernard Thompson <bethomps@redhat.com>
2021-06-09 10:56:17 +0100

    fix: hamburger icon

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,30 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
+import { List } from "phosphor-react";
 import styles from "./NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar className={styles.navBar} variant="light">
-      <Navbar.Brand className={styles.link} href='/'>Bernard Thompson</Navbar.Brand>
-      <Navbar.Toggle aria-controls='basic-navbar-nav' />
-      <Navbar.Collapse id='basic-navbar-nav'>
+    <Navbar className={styles.navBar} expand='lg'>
+      <Navbar.Brand className={styles.link} href='/'>
+        Bernard Thompson
+      </Navbar.Brand>
+      <Navbar.Toggle className={styles.hamburgerIcon} aria-controls='basic-navbar-nav'>
+        <span>
+          <List size={24} color='#ffffff' />
+        </span>
+      </Navbar.Toggle>
+      <Navbar.Collapse id='basic-navbar-nav' className={styles.link}>
         <Nav className='mr-auto'>
-        <Nav.Link className={styles.link} href='/sidebar'>Skills</Nav.Link>
-          <Nav.Link className={styles.link} href='#home'>Experience</Nav.Link>
-          <Nav.Link className={styles.link} href='#link'>Education</Nav.Link>
+          <Nav.Link className={styles.link} href='/sidebar'>
+            Skills
+          </Nav.Link>
+          <Nav.Link className={styles.link} href='#home'>
+            Experience
+          </Nav.Link>
+          <Nav.Link className={styles.link} href='#link'>
+            Education
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
