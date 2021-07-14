2d4ba419d34a4ec68d64be08d02d5d244c0178af
Bernard Thompson <bethomps@redhat.com>
2021-07-14 13:06:28 +0100

    fix: added medium link to profile page

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { TwitterLogo, LinkedinLogo, GithubLogo } from "phosphor-react";
+import { TwitterLogo, LinkedinLogo, GithubLogo, MediumLogo } from "phosphor-react";
 import styles from "../Contact/Contact.module.css";
 
 const Social = () => {
@@ -16,7 +16,9 @@ const Social = () => {
         <div className={styles.socialIcon}>
         <a href="https://twitter.com/bonjo7" target="_blank" rel="noreferrer"><TwitterLogo size={55} color="#1d44b8" /></a>
         </div>
-       
+        <div className={styles.socialIcon}>
+        <a href="https://medium.com/@bethomps" target="_blank" rel="noreferrer"><MediumLogo size={55} color="#1d44b8" /></a>
+        </div>
       </div>
     </div>
   );
