98764489f106d47bafe36edcb4452b2cc7617db7
Bernard Thompson <bethomps@redhat.com>
2021-07-15 17:08:02 +0100

    fix: updated certificate link

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -5,44 +5,48 @@ import styles from "./Education.module.css";
 
 const Education = () => {
   return (
-      <Jumbotron fluid className={styles.background}>
-        <Container>
-          {educationData.map((education, key) => {
-            return (
-              <Card key={key} className={styles.card}>
-                <Card.Body>
-                  <div className={styles.wrapper}>
-                    <div className={styles.imgWrapper}>
-                      <img
-                        src={education.image}
-                        className={styles.img}
-                        alt={`${education.name}s logo`}
-                      />
-                    </div>
-                    <div>
-                      <Card.Title>
-                        <div>{education.course}</div>
-                        <div className={styles.grade}>{education.grade}</div>
-                      </Card.Title>
-                      <Card.Text className={styles.cardText}>
-                        <div>{education.year}</div>
-                        <div>{education.name}</div>
-                        <div>{education?.address}</div>
-                        <div>
+    <Jumbotron fluid className={styles.background}>
+      <Container>
+        {educationData.map((education, key) => {
+          return (
+            <Card key={key} className={styles.card}>
+              <Card.Body>
+                <div className={styles.wrapper}>
+                  <div className={styles.imgWrapper}>
+                    <img
+                      src={education.image}
+                      className={styles.img}
+                      alt={`${education.name}s logo`}
+                    />
+                  </div>
+                  <div>
+                    <Card.Title>
+                      <div>{education.course}</div>
+                      <div className={styles.grade}>{education.grade}</div>
+                    </Card.Title>
+                    <Card.Text className={styles.cardText}>
+                      <div>{education.year}</div>
+                      <div>{education.name}</div>
+                      <div>{education?.address}</div>
+                      <div>
+                        {education.link ? (
                           <a href={education?.link} target='blank'>
                             Online Certificiate
                           </a>
-                        </div>
-                      </Card.Text>
-                      {/* <Button variant='link'>Go somewhere</Button> */}
-                    </div>
+                        ) : (
+                          ""
+                        )}
+                      </div>
+                    </Card.Text>
+                    {/* <Button variant='link'>Go somewhere</Button> */}
                   </div>
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </Container>
-      </Jumbotron>
+                </div>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </Container>
+    </Jumbotron>
   );
 };
 
