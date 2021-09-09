095457a31ee8e7f42c572ba0e56e2298b2abcde9
Bernard Thompson <bethomps@redhat.com>
2021-09-09 12:03:39 +0100

    fix: changed input fields to date values

--- src/components/Experience/UploadExperience.js
+++ src/components/Experience/UploadExperience.js
@@ -16,15 +16,18 @@ const UploadExpereince = () => {
     clearErrors,
   } = useForm();
   const { currentUser } = useAuth();
+  const [employmentStatus, setEmployedStatus] = useState();
   const [formData, setFormData] = useState({});
   const [fileData, setFileData] = useState();
   const [positions, setPositions] = useState([
-    { positionHeldYear: "", positionHeldTitle: "" },
+    { positionStartDate: "", positionFinishDate: "", positionHeldTitle: "" },
   ]);
 
   const { loading, postExperienceData } = UseRoutes();
 
   useEffect(() => {}, [loading]);
+
+  useEffect(() => {}, [employmentStatus]);
 
   const clearErrorsOnInput = (e) => {
     switch (e) {
@@ -73,7 +76,7 @@ const UploadExpereince = () => {
   const handleAddClick = () => {
     setPositions([
       ...positions,
-      { positionHeldYear: "", positionHeldTitle: "" },
+      { positionStartDate: "", positionFinishDate: "", positionHeldTitle: "" },
     ]);
   };
 
@@ -89,191 +92,250 @@ const UploadExpereince = () => {
     postExperienceData(completeFormData);
   };
 
+  const employed = (e) => {
+    if (employmentStatus) {
+      setEmployedStatus(false);
+    } else {
+      setEmployedStatus(true);
+    }
+  };
+
+  console.log(employmentStatus);
   return (
     <>
       {loading ? (
         <Spinner />
       ) : (
         <>
-          <Card className={styles.section}>
-            <Card.Body>
-              <Row>
-                <Col>
-                  <Form.Group className='mb-3' controlId='formBasicEmail'>
-                    <Form.Label>Company Name</Form.Label>
+          <Form>
+            <Card className={styles.section}>
+              <Card.Body>
+                <Row>
+                  <Col>
+                    <Form.Group className='mb-3' controlId='formBasicEmail'>
+                      <Form.Label>Company Name</Form.Label>
 
-                    <Form.Control
-                      className={errors.companyName ? styles.errorsInput : ""}
-                      name='companyName'
-                      type='text'
-                      placeholder='Enter company name'
-                      {...register("companyName", { required: true })}
-                      onChange={(e) => onChange(e)}
-                    />
-                    {errors.companyName && (
-                      <p className={styles.errors}>Company Name is required</p>
-                    )}
-                    <Form.Text className='text-muted'>eg. Red Hat</Form.Text>
-                  </Form.Group>
-                </Col>
-                <Col>
-                  <Form.Group controlId='formFile' className='mb-3'>
-                    <Form.Label>Company Logo</Form.Label>
-                    <Form.Control
-                      className={errors.file ? styles.errorsInput : ""}
-                      type='file'
-                      name='file'
-                      accept='image/*'
-                      {...register("file", { required: true })}
-                      onChange={(e) => onChange(e)}
-                      placeholder='upload image'
-                    />
-                    {errors.file && (
-                      <p className={styles.errors}>Company Image is required</p>
-                    )}
-                  </Form.Group>
-                </Col>
-              </Row>
-
-              <Form.Group className='mb-3' controlId='formBasicEmail'>
-                <Form.Label>Duration (Year)</Form.Label>
-                <Form.Control
-                  className={errors.year ? styles.errorsInput : ""}
-                  name='year'
-                  type='text'
-                  placeholder='Enter year'
-                  {...register("year", { required: true })}
-                  onChange={(e) => onChange(e)}
-                />
-                {errors.year && (
-                  <p className={styles.errors}>Company duration is required</p>
-                )}
-                <Form.Text className='text-muted'>
-                  eg. January 2019 - March 2020
-                </Form.Text>
-              </Form.Group>
-
-              <Form.Group className='mb-3' controlId='formBasicEmail'>
-                <Form.Label>Company Address</Form.Label>
-                <Form.Control
-                  className={errors.address ? styles.errorsInput : ""}
-                  name='address'
-                  type='text'
-                  placeholder='Enter company address'
-                  {...register("address", { required: true })}
-                  onChange={(e) => onChange(e)}
-                />
-                {errors.address && (
-                  <p className={styles.errors}>Company Address is required</p>
-                )}
-                <Form.Text className='text-muted'>
-                  Red Hat, Communications House, Cork Road, Waterford, Ireland.
-                </Form.Text>
-              </Form.Group>
-            </Card.Body>
-          </Card>
-
-          <Card className={styles.section}>
-            <Card.Body>
-              <Form.Group
-                className='mb-3'
-                controlId='formBasicEmail'
-              ></Form.Group>
-              <Row>
-                <Col>
-                  <Form.Label>Year Position Held</Form.Label>
-                </Col>
-                <Col>
-                  <Form.Label>Title Position Held</Form.Label>
-                </Col>
-                <Col></Col>
-              </Row>
-              {positions.map((item, index) => {
-                return (
-                  <Row className={styles.row} key={index}>
-                    <Col>
                       <Form.Control
-                      className={errors.positionHeldYear ? styles.errorsInput : ""}
-                        name='positionHeldYear'
+                        className={errors.companyName ? styles.errorsInput : ""}
+                        name='companyName'
                         type='text'
-                        value={item.positionHeldYear}
-                        placeholder='Enter year(s) you held this position'
-                        onChange={(e) => positionOnChange(e, index)}
+                        placeholder='Enter company name'
+                        {...register("companyName", { required: true })}
+                        onChange={(e) => onChange(e)}
                       />
-                      <Form.Text className='text-muted'>
-                        eg. January 2019 – August 2020
-                      </Form.Text>
-                    </Col>
-                    <Col>
+                      {errors.companyName && (
+                        <p className={styles.errors}>
+                          Company Name is required
+                        </p>
+                      )}
+                      <Form.Text className='text-muted'>eg. Red Hat</Form.Text>
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group controlId='formFile' className='mb-3'>
+                      <Form.Label>Company Logo</Form.Label>
                       <Form.Control
-                        name='positionHeldTitle'
-                        type='text'
-                        value={item.positionHeldTitle}
-                        placeholder='Enter position(s) held'
-                        onChange={(e) => positionOnChange(e, index)}
+                        className={errors.file ? styles.errorsInput : ""}
+                        type='file'
+                        name='file'
+                        accept='image/*'
+                        {...register("file", { required: true })}
+                        onChange={(e) => onChange(e)}
+                        placeholder='upload image'
                       />
-                      <Form.Text className='text-muted'>
-                        eg. Associate Developer
-                      </Form.Text>
-                    </Col>
-
-                    <Col>
-                      {positions.length !== 1 && (
-                        <Trash
-                          className={styles.trash}
-                          size={30}
-                          color='#ed0c0c'
-                          id={item.id}
-                          onClick={(e) => handleRemoveItem(e, index)}
-                        />
+                      {errors.file && (
+                        <p className={styles.errors}>
+                          Company Image is required
+                        </p>
                       )}
-                      {positions.length - 1 === index && (
-                        <Plus
-                          size={30}
-                          color='#1d44b8'
-                          className={styles.addPositionBtn}
-                          onClick={handleAddClick}
+                    </Form.Group>
+                  </Col>
+                </Row>
+
+                <Form.Group className='mb-3' controlId='formBasicEmail'>
+                  <Form.Label>Duration (Year)</Form.Label>
+                  <Form.Control
+                    className={errors.year ? styles.errorsInput : ""}
+                    name='year'
+                    type='text'
+                    placeholder='Enter year'
+                    {...register("year", { required: true })}
+                    onChange={(e) => onChange(e)}
+                  />
+                  {errors.year && (
+                    <p className={styles.errors}>
+                      Company duration is required
+                    </p>
+                  )}
+                  <Form.Text className='text-muted'>
+                    eg. January 2019 - March 2020
+                  </Form.Text>
+                </Form.Group>
+
+                <Form.Group className='mb-3' controlId='formBasicEmail'>
+                  <Form.Label>Company Address</Form.Label>
+                  <Form.Control
+                    className={errors.address ? styles.errorsInput : ""}
+                    name='address'
+                    type='text'
+                    placeholder='Enter company address'
+                    {...register("address", { required: true })}
+                    onChange={(e) => onChange(e)}
+                  />
+                  {errors.address && (
+                    <p className={styles.errors}>Company Address is required</p>
+                  )}
+                  <Form.Text className='text-muted'>
+                    Red Hat, Communications House, Cork Road, Waterford,
+                    Ireland.
+                  </Form.Text>
+                </Form.Group>
+              </Card.Body>
+            </Card>
+
+            <Card className={styles.section}>
+              <Card.Body>
+                <Form.Group
+                  className='mb-3'
+                  controlId='formBasicEmail'
+                ></Form.Group>
+                <Row>
+                  <Col>
+                    <Form.Group className='mb-3' controlId='formBasicCheckbox'>
+                      <Form.Check
+                        type='checkbox'
+                        label='Currently in employment, please click here to set fields correctly'
+                        onClick={(e) => {
+                          employed(e);
+                        }}
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                {positions.map((item, index) => {
+                  console.log(index);
+                  return (
+                    <Row className={styles.row} key={index}>
+                      <Col>
+                        {index === 0 && <Form.Label>Start Date</Form.Label>}
+
+                        <Form.Control
+                          name='positionStartDate'
+                          type='date'
+                          value={item.positionStartDate}
+                          placeholder='Enter year(s) you held this position'
+                          onChange={(e) => positionOnChange(e, index)}
                         />
-                      )}
-                    </Col>
-                  </Row>
-                );
-              })}
-            </Card.Body>
-          </Card>
+                        <Form.Text className='text-muted'>
+                          eg. dd/mm/yyyy
+                        </Form.Text>
+                      </Col>
+                      <Col>
+                        {index === 0 && <Form.Label>Finish Date</Form.Label>}
+                        <Form.Control
+                          name='positionFinishDate'
+                          type={
+                            employmentStatus && index === 0 ? "text" : "date"
+                          }
+                          value={item.positionFinishDate}
+                          placeholder={
+                            employmentStatus && index === 0
+                              ? "Present"
+                              : "dd/mm/yyyy"
+                          }
+                          onChange={(e) => positionOnChange(e, index)}
+                        />
+                        <Form.Text className='text-muted'>
+                          eg.{" "}
+                          {employmentStatus && index === 0
+                            ? "Present"
+                            : "dd/mm/yyyy"}
+                        </Form.Text>
+                      </Col>
+                      <Col>
+                        {index === 0 && <Form.Label>Title</Form.Label>}
+                        <Form.Control
+                          name='positionHeldTitle'
+                          type='text'
+                          value={item.positionHeldTitle}
+                          placeholder='Enter position(s) held'
+                          onChange={(e) => positionOnChange(e, index)}
+                        />
+                        <Form.Text className='text-muted'>
+                          eg. Associate Developer
+                        </Form.Text>
+                      </Col>
 
-          <Card className={styles.section}>
-            <Card.Body>
-              <Form.Group className='mb-3' controlId='formBasicEmail'>
-                <Form.Label>Responsibilities</Form.Label>
-                <Form.Control
-                  className={errors.responsibilities ? styles.errorsInput : ""}
-                  name='responsibilities'
-                  type='text'
-                  as='textarea'
-                  rows={6}
-                  placeholder='Enter responsibilities'
-                  {...register("responsibilities", { required: true })}
-                  onChange={(e) => onChange(e)}
-                />
-                {errors.responsibilities && (
-                  <p className={styles.errors}>Responsibilities is required</p>
-                )}
-                <Form.Text className='text-muted'>
-                  eg. List all your responsibilities
-                </Form.Text>
-              </Form.Group>
-            </Card.Body>
-          </Card>
+                      <Col className={styles.colButton}>
+                        {positions.length !== 1 && (
+                          <Trash
+                            className={
+                              index === 0 ? styles.trashFirst : styles.trash
+                            }
+                            size={30}
+                            color='#ed0c0c'
+                            id={item.id}
+                            onClick={(e) => handleRemoveItem(e, index)}
+                          />
+                        )}
+                      </Col>
+                      <Col className={styles.colButton}>
+                        {positions.length - 1 === index && (
+                          <Plus
+                            className={
+                              index === 0
+                                ? styles.addPositionBtnFirst
+                                : styles.addPositionBtn
+                            }
+                            size={30}
+                            color='#1d44b8'
+                            onClick={handleAddClick}
+                          />
+                        )}
+                      </Col>
+                    </Row>
+                  );
+                })}
+              </Card.Body>
+            </Card>
 
-          <Button
-            style={{ width: "100%" }}
-            variant='success'
-            disabled={currentUser === "TestUser"}
-            onClick={handleSubmit(handleSubmitForm)}
-          >
-            Submit
-          </Button>
+            <Card className={styles.section}>
+              <Card.Body>
+                <Form.Group className='mb-3' controlId='formBasicEmail'>
+                  <Form.Label>Responsibilities</Form.Label>
+                  <Form.Control
+                    className={
+                      errors.responsibilities ? styles.errorsInput : ""
+                    }
+                    name='responsibilities'
+                    type='text'
+                    as='textarea'
+                    rows={6}
+                    placeholder='Enter responsibilities'
+                    {...register("responsibilities", { required: true })}
+                    onChange={(e) => onChange(e)}
+                  />
+                  {errors.responsibilities && (
+                    <p className={styles.errors}>
+                      Responsibilities is required
+                    </p>
+                  )}
+                  <Form.Text className='text-muted'>
+                    eg. List all your responsibilities
+                  </Form.Text>
+                </Form.Group>
+              </Card.Body>
+            </Card>
+
+            <Button
+              style={{ width: "100%" }}
+              variant='success'
+              disabled={currentUser === "TestUser"}
+              onClick={handleSubmit(handleSubmitForm)}
+            >
+              Submit
+            </Button>
+          </Form>
         </>
       )}
     </>
